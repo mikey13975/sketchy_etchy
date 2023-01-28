27b223085728d2cebb87c158427098532dac2526
mikey13975 <mikey13975@gmail.com>
2023-01-27 18:16:20 -0600

    add title, input to index. rough styling stylesheet. create vars for elements

---
+++ sketchyEtchy.js
@@ -0,0 +1,14 @@
+let nmberInput = document.getElementById("nmberBoxes");
+
+let btn = document.getElementById("btn");
+
+let mainContain = document.getElementById("mainContainer");
+
+btn.addEventListener("click", addBoxes);
+
+function addBoxes(nmberInput) {
+	let x = nmberInput;
+	for(i = x, i <= 0, i--) {
+		
+	}
+}
