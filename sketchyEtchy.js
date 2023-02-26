e13a20040b938fc1bb2c45727b3b321217d69a44
mikey13975 <mikey13975@gmail.com>
2023-02-26 02:47:21 -0600

    completed tasks from assignment

--- sketchyEtchy.js
+++ sketchyEtchy.js
@@ -1,14 +1,52 @@
-let nmberInput = document.getElementById("nmberBoxes");
+// var numberInput = document.getElementById("nmbrBoxes").value;
+// console.log(numberInput);
 
-let btn = document.getElementById("btn");
+var btn = document.getElementById("btn");
 
-let mainContain = document.getElementById("mainContainer");
+console.log(btn);
 
-btn.addEventListener("click", addBoxes);
-
-function addBoxes(nmberInput) {
-	let x = nmberInput;
-	for(i = x, i <= 0, i--) {
+function addBoxes(){
+	var usrInput =document.getElementById('input');
+	console.log(usrInput);
+	
+	var x = usrInput.value;	
+	console.log(x);	
+	var mainContain = document.getElementById('main',);
+	
+	for (let i = 0; i < x; i++) {
+		var box = document.createElement('div');
+		box.className = 'colBox';
+		for (let j = 0; j < x; j++ ) {
+			var rowBox = document.createElement('div');
+			
+			rowBox.className = 'rowBox';
+				
+			box.appendChild(rowBox);
+				
+			rowBox.addEventListener("mouseover",function (e) {
+				var randomColor = 'rgb('+ Math.floor(Math.random() * 255) +','+  Math.floor(Math.random() * 255) +','+ Math.floor(Math.random() * 255) +')'; 
+				console.log(randomColor);
+				e.target.style.backgroundColor= randomColor;
+			// console.log(e.target.style.backgroundColor);
+		});
+			
+		}
 		
-	}
-}
+		
+		
+		console.log(i);
+		
+		mainContain.appendChild(box);}
+		console.log(mainContain);		
+		const test = document.getElementsByClassName('colBox');
+		console.log(test+' ,'+ rowBox);
+	};
+	
+	btn.addEventListener("click", addBoxes);
+	
+	
+	// var colored = document.querySelectorAll('rowBox');
+
+	// console.log(colored);
+
+	// addEventListener('mouseover', changeColor);
